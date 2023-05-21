c79a393393510e37f1648144957d12eaea93e6d0
Stepan <stepan@192.168.0.15>
2023-05-21 10:47:41 +0300

    modified lesson2

--- lesson2/main.go
+++ lesson2/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	var str string = "Bld bdld"
+	var a int64 = 5
+	b := 6
 	fmt.Println(str)
 	fmt.Printf("Type: %T Value: %v\n", str, str)
+	fmt.Println(a + int64(b))
 }
