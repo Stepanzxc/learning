fc32e5e3f23285cb52285d2bd0673d0a40292e80
Stepan <stepan@MacBook-Pro-Stepan.local>
2023-05-30 09:18:10 +0300

    make stage 2

--- practis/stage2/main.go
+++ practis/stage2/main.go
@@ -1,14 +1,22 @@
 package main
 
+import "log"
+
 func PositiveSum(numbers []int) int {
-	//TODO:: Твой код
-	return 0
+	x := 0
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] > 0 {
+			x += numbers[i]
+		}
+
+	}
+	return x
 }
 
 func main() {
-	//log.Println(PositiveSum([]int{1, 2, 3, 4, 5}) == 15)
-	//log.Println(PositiveSum([]int{1, -2, 3, 4, 5}) == 13)
-	//log.Println(PositiveSum([]int{}) == 0)
-	//log.Println(PositiveSum([]int{-1, -2, -3, -4, -5}) == 0)
-	//log.Println(PositiveSum([]int{-1, 2, 3, 4, -5})) == 9)
+	log.Println(PositiveSum([]int{1, 2, 3, 4, 5}) == 15)
+	log.Println(PositiveSum([]int{1, -2, 3, 4, 5}) == 13)
+	log.Println(PositiveSum([]int{}) == 0)
+	log.Println(PositiveSum([]int{-1, -2, -3, -4, -5}) == 0)
+	log.Println(PositiveSum([]int{-1, 2, 3, 4, -5}) == 9)
 }
