179d5c1c3478f6f809cac8abee1a4ab633ad3672
Stepan <stepan@MacBook-Pro-Stepan.local>
2023-06-01 11:08:32 +0300

    make stage 9

--- practis/stage9/main.go
+++ practis/stage9/main.go
@@ -1,16 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func Solution(a, b string) string {
-	// TODO: Your code here
-	return ""
+	c := ""
+	if len(a) < len(b) {
+		c = a + b + a
+		fmt.Println(c)
+	} else {
+		c = b + a + b
+	}
+	return c
 }
 
 func main() {
 	log.Println(Solution("45", "1") == "1451")
-	log.Println(Solution("U", "False") == "UFalseU")
 	log.Println(Solution("Soon", "Me") == "MeSoonMe")
+	log.Println(Solution("U", "False") == "UFalseU")
 	log.Println(Solution("13", "200") == "1320013")
 
 }
