51d6d020aff8158e0f2e4b86f100cbc8b40a3cb4
behran <agolovev@gismeteo.ru>
2023-06-03 16:21:27 +0300

    added new stage

---
+++ practis/stage14/main.go
@@ -0,0 +1,15 @@
+package main
+
+// getMaxTimeURL найти максимальное URL по времени выполнения
+func getMaxTimeURL() {
+
+}
+
+// getAvgTimeURL почитать среднее время выполнениея всех запросов
+func getAvgTimeURL() {
+
+}
+
+func main() {
+
+}
