7c22d803d99b3ed72a9a0939921d913d3e6d2979
behran <agolovev@gismeteo.ru>
2023-06-03 12:41:40 +0300

    added new expresions

--- practis/stage11/main.go
+++ practis/stage11/main.go
@@ -7,7 +7,6 @@ import (
 
 // SumMix my favorite functions
 func SumMix(arr []any) int {
-
 	var result []int
 
 	for i := range arr {
@@ -19,17 +18,16 @@ func SumMix(arr []any) int {
 			}
 			result = append(result, strToInt)
 		}
-
 		anyToInt, ok := arr[i].(int)
 		if ok {
 			result = append(result, anyToInt)
 		}
 	}
+
 	var count int
 	for i := range result {
 		count += result[i]
 	}
-
 	return count
 }
 
@@ -38,5 +36,6 @@ func main() {
 	log.Println(SumMix([]any{"5", "0", 9, 3, 2, 1, "9", 6, 7}) == 42)
 	log.Println(SumMix([]any{"1", "5", "8", 8, 9, 9, 2, "3"}) == 45)
 	log.Println(SumMix([]any{8, 0, 0, 8, 5, 7, 2, 3, 7, 8, 6, 7}) == 61)
+	log.Println(SumMix([]any{1, 3}) == 61)
 
 }
