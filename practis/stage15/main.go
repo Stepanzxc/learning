8f5450789c7083552b1494cf14638f6494f3d3ea
Stepan <stepan@MacBook-Pro-Stepan.local>
2023-06-13 17:26:26 +0300

    make stage 15

--- practis/stage15/main.go
+++ practis/stage15/main.go
@@ -1,6 +1,22 @@
+// //Создать функциб которая сделает GET запрос на стороний API и поплучить резульат в формате JSON
+// // https://ipapi.co/176.115.145.30/json/
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
 func main() {
-	//Создать функциб которая сделает GET запрос на стороний API и поплучить резульат в формате JSON
-	// https://ipapi.co/176.115.145.30/json/
+	getJson()
+}
+func getJson() {
+	url, err := http.Get("https://ipapi.co/176.115.145.30/json/")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var result map[string]interface{}
+	json.NewDecoder(url.Body).Decode(&result)
+	log.Println(result)
 }
