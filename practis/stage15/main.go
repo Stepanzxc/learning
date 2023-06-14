dcaaa3bc6479432113f83da8835810eeb4decafd
Stepan <stepan@192.168.0.19>
2023-06-14 21:29:13 +0300

    update stage 15

--- practis/stage15/main.go
+++ practis/stage15/main.go
@@ -4,19 +4,33 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	getJson()
+	fmt.Println(getJson("https://ipapi.co/176.115.145.30/json/"))
 }
-func getJson() {
-	url, err := http.Get("https://ipapi.co/176.115.145.30/json/")
+func getJson(str string) map[string]interface{} {
+	url, err := http.Get(str)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	var result map[string]interface{}
+	var Lastresult map[string]interface{}
 	json.NewDecoder(url.Body).Decode(&result)
-	log.Println(result)
+	Lastresult = result
+	for key, _ := range result {
+		if key == "error" {
+			for key, value := range result {
+				if key == "reason" {
+					fmt.Println("Error: ", value)
+					Lastresult = nil
+					break
+				}
+			}
+		}
+	}
+	return Lastresult
 }
